f50495533243803c21841ee080b546e3689d8b68
Ahmed-Saaed <86948480+Ahmed-Saaed@users.noreply.github.com>
2021-09-11 21:45:13 +0200

    intial commit
    -setup the enviroment
    
    -global varibale
    
    -and post and get request

---
+++ website/app.js
@@ -0,0 +1,10 @@
+/* Global Variables */
+let baseUrl;
+let apiKey;
+
+document.gerElementById("generate").addEventListener("click", myAction);
+
+function myAction(event) {}
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
